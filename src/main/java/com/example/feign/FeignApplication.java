9a89bf7682a7562edbe0dbc779bcd491b25d428f
Michał <michal.przepiorka0@gmail.com>
2020-11-18 21:49:50 +0100

    fix: fix SpringBootApplication bootable

--- src/main/java/com/example/feign/FeignApplication.java
+++ src/main/java/com/example/feign/FeignApplication.java
@@ -1,4 +1,4 @@
-package com.example.feign.infrastructure;
+package com.example.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
